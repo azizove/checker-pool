f2f9899894b56ede204eb750e9609bcde8c1854a
azizove <az.elouarak@gmail.com>
2023-02-27 08:11:03 -0500

    chore: download and upload simple file

--- packages/front/src/components/Checker.tsx
+++ packages/front/src/components/Checker.tsx
@@ -3,6 +3,7 @@ import { useMemo, useRef, useState } from "react";
 import { useCheckerContract } from "../hooks/useCheckerContract";
 import { useTonConnect } from "../hooks/useTonConnect";
 import { Profile } from "../models";
+import axios from 'axios';
 
 import {
   Card,
@@ -19,17 +20,29 @@ import DataTable from "react-data-table-component";
 export function Checker() {
   const { connected } = useTonConnect();
   const { value, address, add } = useCheckerContract();
-  const [fileList, setFileList] = useState<File[]>([]);
+  // const [fileList, setFileList] = useState<File[]>([]);
   const titleRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const statusRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const fileRef = useRef() as React.MutableRefObject<HTMLInputElement>;
-  const handleAddProfile = (e: any) => {
+  const handleAddFile = (e: any) => {
     e.preventDefault();
     // add(new Profile(titleRef.current.value, statusRef.current.value));
-    console.log(fileRef.current.files);
+    if(fileRef.current.files) {
+      const formData = new FormData();
+      const file = fileRef.current.files[0];
+      formData.append('file', file);
+
+      axios.post(import.meta.env.VITE_API_URL+'/api/upload', formData)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   };
   const handleFilesUploadChange = () => {
-      setFileList(fileRef.current?.files ? Array.from(fileRef.current.files) : []);
+      // setFileList(fileRef.current?.files ? Array.from(fileRef.current.files) : []);
     
   }
   /* write a function to transform a filelist to an array */
@@ -66,20 +79,20 @@ export function Checker() {
             {/* <DataTable columns={columns} data={value ?? []} /> */}
           </FlexBoxRow>
           <FlexBoxRow>
-            <StyledForm onSubmit={handleAddProfile}>
+            <StyledForm onSubmit={handleAddFile}>
               <StyledLabel>Name</StyledLabel>
               <StyledInput type="text" ref={titleRef} />
               <StyledLabel>Status</StyledLabel>
               <StyledInput type="text" ref={statusRef} />
               <StyledLabel>File</StyledLabel>
-              <StyledInput type="file" ref={fileRef} multiple onChange={handleFilesUploadChange}/>
-              <ul>
+              <StyledInput type="file" ref={fileRef} onChange={handleFilesUploadChange}/>
+              {/* <ul>
                 {fileList.map((file: any, i: number) => (
                   <li key={i}>
                     {file.name} - {file.type}
                   </li>
                 ))}
-              </ul>
+              </ul> */}
               <Button
                 disabled={!connected}
                 className={`Button ${connected ? "Active" : "Disabled"}`}
--- packages/storage/src/app.js
+++ packages/storage/src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+
+
 const app = express();
 
 // Apply middlware for CORS and JSON endpoing
@@ -9,10 +13,44 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Enable file uploads middleware
+app.use(fileUpload());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Define file upload endpoint
+app.post('/api/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  const file = req.files.file;
+  const fileName = file.name;
+
+  file.mv(`uploads/${fileName}`, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('File uploaded successfully.');
+  });
+});
+
+// Define file download endpoint
+app.get('/api/download/:filename', (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = `uploads/${fileName}`;
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send('File not found.');
+  }
+
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.pipe(res);
+});
+
 app.listen(process.env.PORT, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log(`Storage app listening on port ${process.env.PORT}!`),
 );
